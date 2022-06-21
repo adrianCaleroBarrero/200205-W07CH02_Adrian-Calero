968944f1b91fc7793e688fb7eea3a6d78a262391
adrian <acalerobarrero@gmail.com>
2022-06-21 20:40:06 +0200

    Change controllers for db.json

--- src/controllers/player.controller.ts
+++ src/controllers/player.controller.ts
@@ -1,17 +1,26 @@
-import { iPlayer } from '../interfaces/player';
 import { Request, Response } from 'express';
+import fs from 'fs/promises';
+import { iPlayer } from '../interfaces/player.js';
 
-let players: Array<iPlayer> = [];
-
-export const getController = (req: Request, resp: Response) => {
-    req;
-    resp.setHeader('Content-type', 'application/json');
-    resp.end(JSON.stringify(players));
+const dataFilePath = './src/server/db.json';
+type dbPlayerType = {
+    players: Array<iPlayer>;
 };
 
-export const getControllerId = (req: Request, resp: Response) => {
+export const getController = async (req: Request, resp: Response) => {
+    req;
+    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
     resp.setHeader('Content-type', 'application/json');
-    const result = players.find((player) => player.id === +req.params.id);
+    resp.end(JSON.stringify(JSON.parse(players) as dbPlayerType));
+};
+
+export const getControllerId = async (req: Request, resp: Response) => {
+    resp.setHeader('Content-type', 'application/json');
+    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
+    const search = JSON.parse(players) as dbPlayerType;
+    const result = search.players.find(
+        (player: iPlayer) => player.id === +req.params.id
+    );
     if (result) {
         resp.end(JSON.stringify(result));
     } else {
@@ -20,18 +29,25 @@ export const getControllerId = (req: Request, resp: Response) => {
     }
 };
 
-export const postController = (req: Request, resp: Response) => {
-    const newPlayer = { ...req.body, id: players[players.length - 1].id + 1 };
-    players.push(newPlayer);
-
+export const postController = async (req: Request, resp: Response) => {
+    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
+    const search = JSON.parse(players) as dbPlayerType;
+    const newPlayer = {
+        ...req.body,
+        id: search.players[search.players.length - 1].id + 1,
+    };
+    search.players.push(newPlayer);
+    fs.writeFile(dataFilePath, JSON.stringify(search));
     resp.setHeader('Content-type', 'application/json');
     resp.status(201);
     resp.end(JSON.stringify(newPlayer));
 };
 
-export const patchController = (req: Request, resp: Response) => {
+export const patchController = async (req: Request, resp: Response) => {
+    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
+    const search = JSON.parse(players) as dbPlayerType;
     let modifyPlayer;
-    players = players.map((player) => {
+    search.players = search.players.map((player) => {
         if (player.id === +req.params.id) {
             modifyPlayer = { ...player, ...req.body };
             return modifyPlayer;
@@ -39,13 +55,21 @@ export const patchController = (req: Request, resp: Response) => {
             return player;
         }
     });
+    fs.writeFile(dataFilePath, JSON.stringify(search));
+    resp.setHeader('Content-type', 'application/json');
+    resp.status(200);
+    resp.end(JSON.stringify(modifyPlayer));
 };
 
-export const deleteController = (req: Request, resp: Response) => {
-    const prevLength = players.length;
-    players = players.filter((player) => {
-        player.id !== +req.params.id;
-    });
-    resp.status(prevLength === players.length ? 404 : 202);
+export const deleteController = async (req: Request, resp: Response) => {
+    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
+    const search = JSON.parse(players) as dbPlayerType;
+    const prevLength = search.players.length;
+    search.players = search.players.filter(
+        (player) => player.id !== +req.params.id
+    );
+
+    fs.writeFile(dataFilePath, JSON.stringify(search));
+    resp.status(prevLength === search.players.length ? 404 : 202);
     resp.end(JSON.stringify({}));
 };
