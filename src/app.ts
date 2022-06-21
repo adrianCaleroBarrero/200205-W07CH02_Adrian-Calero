2d70243969a3f26ce63aa78eebcd673467c2fd6c
adrian <acalerobarrero@gmail.com>
2022-06-21 20:39:33 +0200

    Add routing to path /player

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
+import { playerRouter } from './router/player.js';
 
 export const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/player', playerRouter);
