c53ba62c8278490fafc70105000076a883de9f96
adrian <acalerobarrero@gmail.com>
2022-06-23 18:46:38 +0200

    Add & migrate data.model

--- src/controllers/player.controller.ts
+++ src/controllers/player.controller.ts
@@ -1,75 +1,53 @@
+/* eslint-disable no-unused-vars */
 import { Request, Response } from 'express';
 import fs from 'fs/promises';
 import { iPlayer } from '../interfaces/player.js';
+import { PlayerModel } from '../models/player.model.js';
 
 const dataFilePath = './src/server/db.json';
 export type dbPlayerType = {
     players: Array<iPlayer>;
 };
 
-export const getController = async (req: Request, resp: Response) => {
-    req;
-    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
-    resp.setHeader('Content-type', 'application/json');
-    resp.end(JSON.stringify(JSON.parse(players) as dbPlayerType));
-};
+export class playerController {
+    constructor(public model: PlayerModel) {}
 
-export const getControllerId = async (req: Request, resp: Response) => {
-    resp.setHeader('Content-type', 'application/json');
-    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
-    const search = JSON.parse(players) as dbPlayerType;
-    const result = search.players.find(
-        (player: iPlayer) => player.id === +req.params.id
-    );
-    if (result) {
-        resp.end(JSON.stringify(result));
-    } else {
-        resp.status(404);
-        resp.end(JSON.stringify({}));
-    }
-};
+    getAllController = async (req: Request, resp: Response) => {
+        req;
 
-export const postController = async (req: Request, resp: Response) => {
-    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
-    const search = JSON.parse(players) as dbPlayerType;
-    const newPlayer = {
-        ...req.body,
-        id: search.players[search.players.length - 1].id + 1,
+        resp.setHeader('Content-type', 'application/json');
+        resp.end(JSON.stringify(await this.model.findAll()));
     };
-    search.players.push(newPlayer);
-    fs.writeFile(dataFilePath, JSON.stringify(search));
-    resp.setHeader('Content-type', 'application/json');
-    resp.status(201);
-    resp.end(JSON.stringify(newPlayer));
-};
 
-export const patchController = async (req: Request, resp: Response) => {
-    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
-    const search = JSON.parse(players) as dbPlayerType;
-    let modifyPlayer;
-    search.players = search.players.map((player) => {
-        if (player.id === +req.params.id) {
-            modifyPlayer = { ...player, ...req.body };
-            return modifyPlayer;
+    getControllerId = async (req: Request, resp: Response) => {
+        resp.setHeader('Content-type', 'application/json');
+
+        const result = await this.model.find(req.params.id);
+
+        if (result) {
+            resp.end(JSON.stringify(result));
         } else {
-            return player;
+            resp.status(404);
+            resp.end(JSON.stringify({}));
         }
-    });
-    fs.writeFile(dataFilePath, JSON.stringify(search));
-    resp.setHeader('Content-type', 'application/json');
-    resp.status(200);
-    resp.end(JSON.stringify(modifyPlayer));
-};
+    };
 
-export const deleteController = async (req: Request, resp: Response) => {
-    const players = await fs.readFile(dataFilePath, { encoding: 'utf-8' });
-    const search = JSON.parse(players) as dbPlayerType;
-    const prevLength = search.players.length;
-    search.players = search.players.filter(
-        (player) => player.id !== +req.params.id
-    );
+    postController = async (req: Request, resp: Response) => {
+        const newPlayer = await this.model.create(req.body);
+        resp.setHeader('Content-type', 'application/json');
+        resp.status(201);
+        resp.end(JSON.stringify(newPlayer));
+    };
 
-    fs.writeFile(dataFilePath, JSON.stringify(search));
-    resp.status(prevLength === search.players.length ? 404 : 202);
-    resp.end(JSON.stringify({}));
-};
+    patchController = async (req: Request, resp: Response) => {
+        const modifyPlayer = await this.model.update(req.params.id, req.body);
+        resp.setHeader('Content-type', 'application/json');
+        resp.status(200);
+        resp.end(JSON.stringify(modifyPlayer));
+    };
+    deleteController = async (req: Request, resp: Response) => {
+        const { status } = await this.model.delete(req.params.id);
+        resp.status(status);
+        resp.end(JSON.stringify({}));
+    };
+}
---
+++ src/models/player.model.ts
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-vars */
+import fs from 'fs/promises';
+import { iPlayer } from '../interfaces/player.js';
+
+export class PlayerModel {
+    data: Array<iPlayer>;
+    path: string;
+    constructor(private fileName: string) {
+        this.data = [];
+        this.path = `./src/data/${this.fileName}.json`;
+    }
+
+    private async readFile(): Promise<Array<iPlayer>> {
+        return JSON.parse(await fs.readFile(this.path, { encoding: 'utf-8' }));
+    }
+
+    private async writeFile(data: Array<iPlayer>) {
+        return fs.writeFile(this.path, JSON.stringify(data));
+    }
+
+    async findAll(): Promise<Array<iPlayer>> {
+        return this.readFile();
+    }
+
+    async find(id: string): Promise<iPlayer | undefined> {
+        const fileData = await this.readFile();
+        const item = fileData.find((player: iPlayer) => player.id === +id);
+        return item;
+    }
+
+    async create(data: Partial<iPlayer>): Promise<iPlayer> {
+        const fileData = await this.readFile();
+        const newPlayer = {
+            ...data,
+            id: fileData[fileData.length - 1].id + 1,
+        };
+        fileData.push(newPlayer as iPlayer);
+        this.writeFile(fileData);
+        return newPlayer as iPlayer;
+    }
+
+    async update(id: string, data: Partial<iPlayer>): Promise<iPlayer> {
+        let fileData = await this.readFile();
+        if (data.id) delete data.id;
+
+        let modifyPlayer;
+        fileData = fileData.map((player) => {
+            if (player.id === +id) {
+                modifyPlayer = { ...player, ...data };
+                return modifyPlayer;
+            } else {
+                return player;
+            }
+        });
+        this.writeFile(fileData);
+        return modifyPlayer as unknown as iPlayer;
+    }
+
+    async delete(id: string) {
+        let fileData = await this.readFile();
+        const prevLength = fileData.length;
+        fileData = fileData.filter((player) => player.id !== +id);
+        if (prevLength === fileData.length) return { status: 404 };
+        this.writeFile(fileData);
+        return { status: 202 };
+    }
+}
